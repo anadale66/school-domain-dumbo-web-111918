8e484850181f687728a321ef8ddb8944e4a88c4e
Flatiron School <flatironschool@Flatirons-MacBook-Air-10.local>
2018-11-23 19:10:47 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student,grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each_value do |names|
+      names.sort!
+    end
+  end
+
+
+end
